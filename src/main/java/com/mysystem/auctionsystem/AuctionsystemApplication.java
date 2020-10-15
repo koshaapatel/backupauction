262637cd428be3b9c71317f8fb2d077c77252797
koshaapatel <koshapatel1045@gmail.com>
2020-10-15 19:43:51 -0400

    Spring boot project initialised

---
+++ src/main/java/com/mysystem/auctionsystem/AuctionsystemApplication.java
@@ -0,0 +1,13 @@
+package com.mysystem.auctionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysystem/auctionsystem/AuctionsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysystem.auctionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
